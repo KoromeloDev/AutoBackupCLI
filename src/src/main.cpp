822bf76006367e6ba41f32d88070025caeb26898
KoromeloDev <koromelo.d@gmail.com>
2025-01-22 03:22:59 +0300

    Base commit

---
+++ src/src/main.cpp
@@ -0,0 +1,49 @@
+#include <QCoreApplication>
+#include <QDir>
+
+//Creates folders in the current directory
+void createPath(const QVector<QString> &pathList)
+{
+  for (QString path : pathList)
+  {
+    path = QDir::currentPath() + "/" + path;
+
+    if(!QDir(path).exists())
+    {
+      const bool success = QDir(path).mkdir(path);
+
+      if (!success)
+      {
+        qDebug() << "Failed to create directory: " << path;
+      }
+    }
+  }
+}
+
+//Creating a folder with configuration files
+void setPath()
+{
+  QString path;
+
+  path.append(QDir::homePath());
+  path.append("/.config/");
+  path.append(PROJECT_NAME);
+
+  if(!QDir(path).exists())
+  {
+    if (!QDir(path).mkdir(path))
+    {
+      qDebug() << "Failed to create directory: " << path;
+    }
+  }
+
+  QDir::setCurrent(path);
+}
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication application(argc, argv);
+  QCoreApplication::setApplicationVersion(APP_VERSION "-beta");
+  setPath();
+  return QCoreApplication::exec();
+}
